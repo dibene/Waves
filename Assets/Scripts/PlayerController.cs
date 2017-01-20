b2e363d16377d2cad5af7c94e068e6eea6534d34
dibene santiago <dibenesantiago@gmail.com>
2017-01-20 20:52:19 -0300

    second commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody m_Rigidbody;         
+
+
+	void Awake () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
