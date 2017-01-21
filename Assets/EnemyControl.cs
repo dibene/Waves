84279c864409776ebbb948f2914d5be2b091f78e
dibene santiago <dibenesantiago@gmail.com>
2017-01-21 01:39:04 -0300

    enemy?

---
+++ Assets/EnemyControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+	public float moveSpeed;
+
+	private Rigidbody2D m_Rigidbody;         
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			m_Rigidbody.velocity = new Vector2 (moveSpeed, m_Rigidbody.velocity.y);
+
+	}
+}
